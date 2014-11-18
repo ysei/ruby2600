39cfa46709923006ffb03fd03dc1315af889b13a
baron <baron@users>
2014-11-18 14:10:00 +0000

    Warizan

--- lib/ruby2600/cpu.rb
+++ lib/ruby2600/cpu.rb
@@ -447,14 +447,16 @@ module Ruby2600
 
     def bcd_to_value(value)
       return value unless @d
-      ([value / 16, 9].min) * 10 + ([value % 16, 9].min)
+#     ([value / 16, 9].min) * 10 + ([value % 16, 9].min)
+      ([value * 0.0625, 9].min) * 10 + ([value % 16, 9].min)
     end
 
     def value_to_bcd(value)
       return value unless @d
       value = 100 + value if value < 0
       value -= 100        if value > 99
-      (value / 10) * 16 + (value % 10)
+#     (value / 10) * 16 + (value % 10)
+      value * 1.6 + (value % 10)
     end
 
     # Convenience conversions (most due to the lack of byte/word & signed/unsigned types)
